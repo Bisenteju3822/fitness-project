fe4c04d8095c2b80807aa81843695fc010e082e5
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-03-04 09:24:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,27 @@ const cors = require("cors");
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const fitnessRoute = require("./routes/fitnessRoute");
-const learnerRoute = require("./routes/learnerRoute")
+const learnerRoute = require("./routes/learnerRoute");
 require("dotenv").config();
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-mongoose.connect(process.env.DBCONN).then(() => {
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DBCONN, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+}).then(() => {
   console.log("DB connected!!!");
-})
+}).catch((err) => {
+  console.error("DB connection error: ", err);
+});
+
 app.use("/fitness", fitnessRoute);
-app.use("/learner", learnerRoute)
+app.use("/learner", learnerRoute);
+
 const Port = process.env.PORT || 8080;
 app.listen(Port, () => {
-  console.log(`server run on  port ${Port}`);
-})
+  console.log(`Server running on port ${Port}`);
+});
